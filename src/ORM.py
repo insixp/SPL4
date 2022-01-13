92629f89b0d1248480e18c026390d3580fee41ee
Tal Gonen <talgonen16@gmail.com>
2022-01-13 14:06:25 +0200

    removed src folder

--- src/ORM.py
+++
@@ -1,7 +0,0 @@
-import inspect
-
-def orm(cursor, dto_type):
-    args = inspect.getfullargspec(dto_type.__init__).args[1:0] 
-    col_names = [column[0] for column in cursor.description]
-    col_mapping = [col_names.index(arg) for arg in args]
-    return [row_map(row, col_mapping, dto_type) for row in cursor.fetchall()]
