40d70523be80c7fca976429e5a24ccceec8373b9
Tal Gonen <talgonen16@gmail.com>
2022-01-13 14:06:06 +0200

    final chupa

---
+++ DTO.py
@@ -0,0 +1,20 @@
+class Hat:
+    def __init__(self, id, topping, supplier, quantity):
+        self.id = id
+        self.topping = topping
+        self.supplier = supplier
+        self.quantity = quantity
+
+class Supplier:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def __cmp__(self, other):
+        return self.supplier > other.supplier
+
+class Order:
+    def __init__(self, id, location, hat):
+        self.id = id
+        self.location = location
+        self.hat = hat
